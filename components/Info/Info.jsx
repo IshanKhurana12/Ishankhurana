be40e780d2dc07d7467ed6b017bb6064d4793923
Ishan khurana <87608303+IshanKhurana12@users.noreply.github.com>
2023-08-01 19:53:13 +0530

    Update Info.jsx

--- components/Info/Info.jsx
+++ components/Info/Info.jsx
@@ -3,20 +3,8 @@ import React from 'react'
 import style from './info.module.css';
 const Info = ({value}) => {
     
-  return (
-    <div>
-      <h1 id={style.head}>{value.name}</h1>
-      <p style={{color:'white'}} id={style.para}>{value.data}</p>
-
-      <br />
-      <a href={value.Github}>Github &nbsp;</a>   
-       <a href={value.link}>&nbsp; ViewSite</a>
-
-      <br />
-      
-        
-    </div>
-  )
+  return(
+      <></>
 }
 
 export default Info
